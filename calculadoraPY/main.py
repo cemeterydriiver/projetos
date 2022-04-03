cf6b6b1e2ac4006870533952d0197fe72d56a6e5
anaclaradsouza2005@gmail.com <anaclaradsouza2005@gmail.com>
2022-04-03 14:48:57 -0300

    projetos

---
+++ calculadoraPY/main.py
@@ -0,0 +1,9 @@
+#O FUNCIONAMENTO DA CALCULADORA É VIA TERMINAL OU RODANDO O CODE NO VSCODE!#
+
+expressao = input('Digite aqui  uma expressão matemática')
+
+resultado = eval(expressao)
+
+print('o resultado é', resultado )
+
+#XOXO: ash <3#
