d601f53aba60268113ea9b7055638a7f9db7ccdc
ash <anaclaradsouza2005@gmail.com>
2022-04-04 19:03:52 -0300

    adicionando megasena.java

---
+++ megasenacopy/src/megasena.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+
+public class megasena {
+    public static void main(String[] args) {
+        Random generate = new Random();
+
+        int i = 0;
+        while (i < 6) {
+            int number = generate.nextInt(60);
+            System.out.println(number);
+            i++;
+        }
+    }
+}
