86fd63015c9105366e9d4cc469952bd8cdd824ac
ash <anaclaradsouza2005@gmail.com>
2022-06-07 20:13:00 -0300

    07.06

--- Calculadora IMC/src/br/unisal/aula/exception/IMCNumeroNegativoException.java
+++
@@ -1,9 +0,0 @@
-package br.unisal.aula.exception;
-
-
-public class IMCNumeroNegativoException extends RuntimeException {
-
-    public IMCNumeroNegativoException() {
-        super("Peso ou altura negativos");
-    }
-}
--- calculadoraPY/main.py
+++
@@ -1,9 +0,0 @@
-#O FUNCIONAMENTO DA CALCULADORA É VIA TERMINAL OU RODANDO O CODE NO VSCODE!#
-
-expressao = input('Digite aqui  uma expressão matemática')
-
-resultado = eval(expressao)
-
-print('o resultado é', resultado )
-
-#XOXO: ash <3#
